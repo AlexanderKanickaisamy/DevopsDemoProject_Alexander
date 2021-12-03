8fb6a0dbf3f0af9df2da41e97493dfac7b8015f9
AlexanderKanickaisamy <antonyalexander65@gmail.com>
2021-12-04 01:15:25 +0530

    Update variable.tf

--- App-Code/testapp-code/terraform-code/variable.tf
+++ App-Code/testapp-code/terraform-code/variable.tf
@@ -11,26 +11,10 @@ variable "sg_id" {
   default = "sg-0ae75bebd1b5680ec"
 }
 
-
 variable "ami_id" {
-  type = map
-
-  default = {
-    "us-east-1" = "ami-0ba62214afa52bec7"
-    "us-west-1" = "ami-0ba62214afa52bec7"
-  }
+  default = "ami-0ba62214afa52bec7"
 }
 
-variable "instance_count" {
-  default = "2"
-}
-
-variable "instance_tags" {
-  type = list
-  default = ["Terraform-1", "Terraform-2"]
-}
-
-variable "aws_region" {
+variable "region" {
   default = "us-east-2"
 }
-
